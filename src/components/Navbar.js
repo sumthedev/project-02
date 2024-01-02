7382abef975275230c9cd7059e2b0f9f1f53ede6
Sumbal Hashmi <115552450+sumthedev@users.noreply.github.com>
2024-01-02 18:09:51 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,8 @@ function Navbar() {
   return (
     <>
       <div className="flex items-center bg-04-shadow  p-[10px] justify-between">
-        {/* Left side of the Navbar */}
         <div className="flex flex-grow">
-          {/* Search bar */}
-          <div
-            className="search-bar flex flex-row bg-03-shadow">
+          <div className="search-bar flex flex-row bg-03-shadow">
             <div className="flex flex-row">
               <span className="ml-5 mt-1">
                 <img src="/images/search-normal.svg" alt="search" />
@@ -22,24 +19,18 @@ function Navbar() {
           </div>
         </div>
 
-        {/* Right side of the Navbar */}
-        <div className="flex right-nav flex-row">
-          {/* Notification icon */}
-          <span className="">
-            <img src="/images/img-01.svg" alt="notification" />
-          </span>
+        <span className="">
+          <img src="/images/img-01.svg" alt="notification" />
+        </span>
 
-          {/* User info */}
-          <div className="flex flex-col">
-            <span className="text-02 mt-[10px]">John Doe</span>
-            <span className="text-03">Admin</span>
-          </div>
-
-          {/* Admin icon */}
-          <span className="">
-            <img src="/images/admin.svg" alt="admin" />
-          </span>
+        <div className="flex flex-col p-[10px]">
+          <span className="text-02 mt-[10px]">John Doe</span>
+          <span className="text-03">Admin</span>
         </div>
+
+        <span className="">
+          <img src="/images/admin.svg" alt="admin" />
+        </span>
       </div>
     </>
   );
