873ab9b14c63af663f2b587dd60a782e5634f233
Sumbal Hashmi <115552450+sumthedev@users.noreply.github.com>
2024-01-09 23:51:14 +0500

    Update View.js

--- src/Pages/View.js
+++ src/Pages/View.js
@@ -6,79 +6,77 @@ function Eventview() {
   return (
     <>
       <div className="flex flex-col mt-[50px] w-full p-[10px]">
-
         <div className="flex w-full flex-row bg-02 mt-[20px] ">
           <div className="flex ml-[20px] mt-[12px]">
-
-        <div className="flex flex-row bg-02 mt-[20px] w-full">
-          <div className="flex mt-[12px] ml-[20px]">
-
-            <span>
-              <img src="/images/event.png" alt="view" />
-            </span>
-          </div>
-          <div>
-            <div className="flex flex-col ml-[20px] mt-[20px]">
-              <span className="text-black text-13 flex ml-[8px]">
-                Vacation Event
-              </span>
-              <span className=" flex-row text-14 ml-[8px] mt-[5px] flex">
-                <span className="flex ">Created By:</span>
-                <span className="flex ml-[3px] text-black"> joedavid</span>
-              </span>
-              <span className=" flex-row text-14 ml-[8px] mt-[5px] flex">
-                <span className="flex ">Event Type:</span>
-                <span className="flex ml-[3px] text-black"> PlayJoy</span>
-              </span>
-              <div className="flex mt-[8px] ml-[-5px]  flex-row w-[753px]  h-[97px]">
-                <div className="flex item-01 w-[167px] h-[77px] ml-[8px] mt-[10px] bg-white">
-                  <div className="flex flex-col mt-[12px] ml-[29px]">
-                    <span className="text-15">Islamabad</span>
-                    <span className="text-16 flex mt-[8px]">Location</span>
-                  </div>
-                </div>
-                <div className="flex item-01 w-[167px] h-[77px] ml-[8px] mt-[10px]">
-                  <div className="flex flex-col mt-[12px] ml-[29px]">
-                    <span className="text-17">10 SEP</span>
-                    <span className="text-18 flex mt-[8px]">Date</span>
-                  </div>
-                </div>
-                <div className="flex item-01 w-[167px] h-[77px] ml-[8px] mt-[10px]">
-                  <div className="flex flex-col mt-[12px] ml-[29px]">
-                    <span className="text-17">20000</span>
-                    <span className="text-18 flex mt-[8px]">Comments</span>
-                  </div>
-                </div>
-                <div className="flex item-01 w-[167px] h-[77px] ml-[8px] mt-[10px]">
-                  <div className="flex flex-col mt-[12px] ml-[29px]">
-                    <span className="text-17">200</span>
-                    <span className="text-18 flex mt-[8px]">Interested</span>
-                  </div>
-                </div>
-                <div className="flex item-01 w-[167px] h-[77px] ml-[8px] mt-[10px]">
-                  <div className="flex flex-col mt-[12px] ml-[29px]">
-                    <span className="text-17">200</span>
-                    <span className="text-18 flex mt-[8px]">Going</span>
+            <div className="flex flex-row bg-02 mt-[20px] w-full">
+              <div className="flex mt-[12px] ml-[20px]">
+                <span>
+                  <img src="/images/event.png" alt="view" />
+                </span>
+              </div>
+              <div>
+                <div className="flex flex-col ml-[20px] mt-[20px]">
+                  <span className="text-black text-13 flex ml-[8px]">
+                    Vacation Event
+                  </span>
+                  <span className=" flex-row text-14 ml-[8px] mt-[5px] flex">
+                    <span className="flex ">Created By:</span>
+                    <span className="flex ml-[3px] text-black"> joedavid</span>
+                  </span>
+                  <span className=" flex-row text-14 ml-[8px] mt-[5px] flex">
+                    <span className="flex ">Event Type:</span>
+                    <span className="flex ml-[3px] text-black"> PlayJoy</span>
+                  </span>
+                  <div className="flex mt-[8px] ml-[-5px]  flex-row w-[753px]  h-[97px]">
+                    <div className="flex item-01 w-[167px] h-[77px] ml-[8px] mt-[10px] bg-white">
+                      <div className="flex flex-col mt-[12px] ml-[29px]">
+                        <span className="text-15">Islamabad</span>
+                        <span className="text-16 flex mt-[8px]">Location</span>
+                      </div>
+                    </div>
+                    <div className="flex item-01 w-[167px] h-[77px] ml-[8px] mt-[10px]">
+                      <div className="flex flex-col mt-[12px] ml-[29px]">
+                        <span className="text-17">10 SEP</span>
+                        <span className="text-18 flex mt-[8px]">Date</span>
+                      </div>
+                    </div>
+                    <div className="flex item-01 w-[167px] h-[77px] ml-[8px] mt-[10px]">
+                      <div className="flex flex-col mt-[12px] ml-[29px]">
+                        <span className="text-17">20000</span>
+                        <span className="text-18 flex mt-[8px]">Comments</span>
+                      </div>
+                    </div>
+                    <div className="flex item-01 w-[167px] h-[77px] ml-[8px] mt-[10px]">
+                      <div className="flex flex-col mt-[12px] ml-[29px]">
+                        <span className="text-17">200</span>
+                        <span className="text-18 flex mt-[8px]">
+                          Interested
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex item-01 w-[167px] h-[77px] ml-[8px] mt-[10px]">
+                      <div className="flex flex-col mt-[12px] ml-[29px]">
+                        <span className="text-17">200</span>
+                        <span className="text-18 flex mt-[8px]">Going</span>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
+
+              <div className="flex back-color mt-[20px]  ml-[400px] w-[50px] h-[33px]">
+                <button>
+                  <span className="flex ml-[12px]  text-sm text-white">
+                    <Link to="/event-edit">Edit</Link>
+                  </span>
+                </button>
+                <button className="">
+                  <span className="flex ml-[20px] w-[27px] h-[27px]">
+                    <img src="/images/view-02.png" alt="view" />
+                  </span>
+                </button>
+              </div>
             </div>
-          </div>
-
-          <div className="flex back-color mt-[20px]  ml-[400px] w-[50px] h-[33px]">
-
-          <div className="flex back-color mt-[20px]  ml-[50px] w-[50px] h-[33px]">
-
-            <button>
-              <span className="flex ml-[12px]  text-sm text-white">
-                <Link to="/event-edit">Edit</Link>
-              </span>
-            </button>
-            <button className="">
-              <span className="flex ml-[20px] w-[27px] h-[27px]">
-                <img src="/images/view-02.png" alt="view" />
-              </span>
-            </button>
           </div>
         </div>
         <div className="bg-02 w-[1005] mt-[20px] flex flex-row h-[390px]">
